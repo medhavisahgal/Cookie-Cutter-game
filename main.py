0d008474f1c45a1b4a62b8b62f2b5ef6c3a43419
medhavisahgal <112403556+medhavisahgal@users.noreply.github.com>
2023-04-16 18:20:37 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,94 @@
+import cv2
+from cvzone import HandTrackingModule, overlayPNG
+import numpy as np
+import os
+folderPath='frames'
+mylist = os.listdir(folderPath)
+graphic = [cv2.imread(f'{folderPath}/{imPath}') for imPath in mylist]
+intro =graphic[0];
+kill =graphic[1];
+winner = graphic[2];
+cam = cv2.VideoCapture(0)
+detector = HandTrackingModule.HandDetector(maxHands=1,detectionCon=0.77)
+cv2.imshow('Squid Game', cv2.resize(intro, (0, 0), fx=0.69, fy=0.69))
+cv2.waitKey(1)
+while True:
+    cv2.imshow('Squid Game', cv2.resize(intro, (0, 0), fx=0.69, fy=0.69))
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+TIMER_MAX = 45
+TIMER = TIMER_MAX
+cap = cv2.VideoCapture(0)
+frameHeight = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+frameWidth = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+#sets the minimum confidence threshold for the detection
+#INITILIZING GAME COMPONENTS
+#----------------------------------------------------------------
+sqr_img = cv2.imread('img/sqr(2).png')
+mlsa =  cv2.imread('img/mlsa.png')
+#INTRO SCREEN WILL STAY UNTIL Q IS PRESSED
+gameOver = False
+NotWon =True
+#GAME LOGIC UPTO THE TEAMS
+from cvzone import HandTrackingModule, overlayPNG
+
+capture = cv2.VideoCapture(0)
+detector = HandTrackingModule.HandDetector(maxHands=2, detectionCon=0.77)
+## max hands for no of hands we need to detect
+## detectionCon for percentage of error we can allow. Range is from 0 to 1
+
+while True:
+    isTrue, frame = capture.read()
+    hands, img = detector.findHands(frame, flipType=True)
+
+    if hands:
+        # Hand 1
+        hand1 = hands[0]
+        lmList1 = hand1["lmList"]  # List of 21 Landmark points
+        bbox1 = hand1["bbox"]  # Bounding box info x,y,w,h
+        centerPoint1 = hand1['center']  # center of the hand cx,cy
+        handType1 = hand1["type"]  # Handtype Left or Right
+
+        fingers1 = detector.fingersUp(hand1)
+
+        if len(hands) == 2:
+            # Hand 2
+            hand2 = hands[1]
+            lmList2 = hand2["lmList"]  # List of 21 Landmark points
+            bbox2 = hand2["bbox"]  # Bounding box info x,y,w,h
+            centerPoint2 = hand2['center']  # center of the hand cx,cy
+            handType2 = hand2["type"]  # Hand Type "Left" or "Right"
+
+            fingers2 = detector.fingersUp(hand2)
+
+    cv2.imshow('Video', frame)
+    if(cv2.waitKey(20) & 0xFF==ord('q')):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
+#-----------------------------------------------------------------------------------------
+while not gameOver:
+        continue
+#LOSS SCREEN
+if NotWon:
+    for i in range(10):
+       cv2.imshow('Squid Game', cv2.resize(kill, (0, 0), fx=0.69, fy=0.69))
+    while True:
+        cv2.imshow('Squid Game', cv2.resize(kill, (0, 0), fx=0.69, fy=0.69))
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+else:
+    cv2.imshow('Squid Game', cv2.resize(winner, (0, 0), fx=0.69, fy=0.69))
+    cv2.waitKey(125)
+
+    while True:
+        cv2.imshow('Squid Game', cv2.resize(winner, (0, 0), fx=0.69, fy=0.69))
+        # cv2.imshow('shit',cv2.resize(graphic[3], (0, 0), fx = 0.5, fy = 0.5))
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+cv2.destroyAllWindows()
+
+#destroy all the windows
